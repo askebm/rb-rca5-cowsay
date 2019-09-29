cb23f87f5d8288bec994f7ac1a0a004d26953bb5
Aske Bækdal Møller <asmoe16@student.sdu.dk>
2019-09-29 09:10:49 +0200

    Enabled ROS functionality

---
+++ gazebo_ros_server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Call: ./gazebo_server world.world
+
+DIR="$( cd "$( dirname "$0" )" >/dev/null && pwd )"
+echo $DIR
+export GAZEBO_MODEL_PATH=$DIR/models/:$GAZEBO_MODEL_PATH 
+
+#export GAZEBO_PLUGIN_PATH=/usr/lib/x86_64-linux-gnu/gazebo-10/plugins/:$GAZEBO_PLUGIN_PATH
+export GAZEBO_PLUGIN_PATH=$DIR/marble_contact_plugin/build:$GAZEBO_PLUGIN_PATH
+export GAZEBO_RESOURCE_PATH=${DIR}
+
+roslaunch verbose:=true "$@"
+
