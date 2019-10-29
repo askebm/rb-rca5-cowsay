b2c6a31ee9394fc0566db329bc4c94d4e9b42860
Aske B.M <asmoe16@student.sdu.dk>
2019-10-29 18:26:09 +0100

    Fixed using CMake librarys

--- SmartAssRobot/Mains/marbleDetection1.cpp
+++ SmartAssRobot/Mains/marbleDetection1.cpp
@@ -6,14 +6,16 @@
 #include <gazebo/transport/transport.hh>
 
 #include <opencv2/opencv.hpp>
-#include "../Classes/Camera/Vision.h"
+//#include "../Classes/Camera/Vision.h"
+#include <Camera/Vision.h>
 
 int main()
 {
     
     
+	Vision vis;
 
-    cv::imshow("test",)
+//    cv::imshow("test",)
 
     
 
