3ceab481dcce3af9f31d40c79424c46e1f59b1bf
AC12344 <annec.poulsen@gmail.com>
2019-11-01 10:53:11 +0100

    added lidar path_planning as a lib. nothign in it yet

---
+++ SmartAssRobot/Classes/Lidar/path_planning.cpp
@@ -0,0 +1,6 @@
+#include "path_planning.h"
+
+Path_planning::Path_planning()
+{
+
+}
---
+++ SmartAssRobot/Classes/Lidar/path_planning.h
@@ -0,0 +1,16 @@
+#ifndef PATH_PLANNING_H
+#define PATH_PLANNING_H
+
+#include <gazebo/gazebo_client.hh>
+#include <gazebo/msgs/msgs.hh>
+#include <gazebo/transport/transport.hh>
+
+#include <opencv2/opencv.hpp>
+
+class Path_planning
+{
+public:
+    Path_planning();
+};
+
+#endif // PATH_PLANNING_H
