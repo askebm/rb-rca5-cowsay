d09e0082d29f68ef43c1e372ffb4c86c07d9330e
AC12344 <annec.poulsen@gmail.com>
2019-11-02 15:34:39 +0100

    Started the location tracking. Implemented the least square method  for computing the line and the incremental algorithm as it seemed to be the easiest one. It is not working a 100% yet.

--- SmartAssRobot/Classes/Lidar/path_planning.h
+++ SmartAssRobot/Classes/Lidar/path_planning.h
@@ -5,12 +5,28 @@
 #include <gazebo/msgs/msgs.hh>
 #include <gazebo/transport/transport.hh>
 
-#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <vector>
+#include <math.h>       /* atan2 */
+
+using namespace std;
 
 class Path_planning
 {
 public:
     Path_planning();
+    ~Path_planning();
+    void localization();
+    vector<vector<float>> lineExtraction(vector<float> p, vector<float> tetha, int n ); // Incremental algorithm
+    void leastSquare(vector<float> p, vector<float> tetha, int n );
+    void rawData(float range, float angle, int counter);
+
+private:
+    vector<vector<float>> points;
+    float r;
+    float alpha;
+    double threshhold_d = 5;
+
 };
 
 #endif // PATH_PLANNING_H
